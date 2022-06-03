729237565032496f11618588ff4efdf1bb4a35cc
Sherman Au <shermau13@gmail.com>
2022-06-03 05:22:04 -0600

    refactor: change back to passing test now circleci is happy

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,6 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
-    it { should validate_uniqueness_of(:boom) }
+    it { should validate_uniqueness_of(:email) }
   end
 end
