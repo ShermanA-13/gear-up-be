79dcd72fe779e7cfafb8f770e86bef12b953ef99
Sherman Au <shermau13@gmail.com>
2022-06-03 16:41:32 -0600

    add: factoybot to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'factory_bot_rails'
 
 users = FactoryBot.create_list(:user, 10)
 
