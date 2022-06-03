8ebdb100c19d606a8bb7fcc00fa369d67ed6aab2
Susan Boyd <sueboyd922@gmail.com>
2022-06-03 15:36:17 -0400

    Test: happy path for one user by id

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -21,4 +21,25 @@ RSpec.describe "Users API" do
       end
     end
   end
+
+  describe 'get one user' do
+    it 'can get a user by id' do
+      users_list = create_list(:user, 3)
+
+      get "/api/v1/users/#{users_list.first.id}"
+
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      user = user_response[:data]
+
+      expect(response).to be_successful
+
+      expect(user[:id]).to eq(users_list.first.id)
+      expect(user[:type]).to eq("user")
+      expect(user[:attributes][:first_name]).to eq(users_list.first.first_name)
+      expect(user[:attributes][:last_name]).to eq(users_list.first.last_name)
+      expect(user[:attributes][:email]).to eq(users_list.first.email)
+    end
+  end
+
+
 end
