709d6edd4fad4784f7b1ca3e4bebbc6faf806acd
Sherman Au <shermau13@gmail.com>
2022-06-02 21:45:27 -0600

    test: user validations/relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should have_many(:items) }
+    it { should have_many(:trip_items).through(:items) }
+    it { should have_many(:trip_users) }
+    it { should have_many(:trips).through(:trip_users) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_uniqueness_of(:email) }
+  end
 end
