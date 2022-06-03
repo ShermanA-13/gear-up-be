4f0c5a355521572b42e56ba5752bbf9871871fcd
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-03 16:15:07 -0600

    Create item enums

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,17 @@ class Item < ApplicationRecord
 
   validates_presence_of :name, :category
   validates_numericality_of :count, greater_than: 0
+
+  enum status:{
+    "Tents" => 1,
+    "Food" => 2,
+    "Sleeping Bag" => 3,
+    "Pads & Hammocks" => 4,
+    "Stoves, Grills & Fuel" => 5,
+    "Dishes" => 6,
+    "Ropes" => 7,
+    "Harnesses" => 8,
+    "Belay & Rappel" => 9,
+    "Crash Pads" => 10
+  }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
-FactoryBot.define do
-  factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.safe_email(name: first_name) }
-  end
-end
+# FactoryBot.define do
+#   factory :user do
+#     first_name { Faker::Name.first_name }
+#     last_name { Faker::Name.last_name }
+#     email { Faker::Internet.safe_email(name: first_name) }
+#   end
+# end
