e875adbc34ecc61fcca45aaa356eb1a0d298381f
Susan Boyd <sueboyd922@gmail.com>
2022-06-02 22:16:41 -0400

    Test: extra validation for start/end date on Trip

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -15,5 +15,8 @@ RSpec.describe Trip, type: :model do
     it { should validate_presence_of :end_date }
     it { should validate_presence_of :description }
     it { should validate_presence_of :host_id }
+
+    # it { should validate_inclusion_of(:start_date).in_range(Date.today..Date.today.next) }
+
   end
 end
