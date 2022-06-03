73f795775b5cead213cfb6b1c6d343cd01bccab9
Susan Boyd <sueboyd922@gmail.com>
2022-06-03 18:12:11 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-FactoryBot.create_list(:user, 10)
+users = FactoryBot.create_list(:user, 10)
+
+FactoryBot.create_list(:item, 5, user: users[0])
+FactoryBot.create_list(:item, 8, user: users[1])
+FactoryBot.create_list(:item, 3, user: users[2])
+FactoryBot.create_list(:item, 5, user: users[3])
+FactoryBot.create_list(:item, 1, user: users[4])
+FactoryBot.create_list(:item, 5, user: users[5])
+FactoryBot.create_list(:item, 6, user: users[6])
+FactoryBot.create_list(:item, 5, user: users[7])
+FactoryBot.create_list(:item, 2, user: users[8])
+FactoryBot.create_list(:item, 5, user: users[9])
+
+FactoryBot.create_list(:trip, 3)
---
+++ spec/factories/trip.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :trip do
+    name { Faker::Lorem.word }
+    location { Faker::Movies::HarryPotter.location }
+    start_date { Date.today }
+    end_date { Date.today.next_day }
+    description { Faker::Movies::HarryPotter.quote }
+    host_id { Faker::Number.between(from: 1, to: 10) }
+  end
+end
