b8ae424e969445060b2b995ca1553c63d07870c2
Sherman Au <shermau13@gmail.com>
2022-06-03 16:50:38 -0600

    heroku test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'factory_bot_rails'
 
-users = FactoryBot.create_list(:user, 10)
+# users = FactoryBot.create_list(:user, 10)
 
-FactoryBot.create_list(:item, 5, user: users[0])
-FactoryBot.create_list(:item, 8, user: users[1])
-FactoryBot.create_list(:item, 3, user: users[2])
-FactoryBot.create_list(:item, 5, user: users[3])
-FactoryBot.create_list(:item, 1, user: users[4])
-FactoryBot.create_list(:item, 5, user: users[5])
-FactoryBot.create_list(:item, 6, user: users[6])
-FactoryBot.create_list(:item, 5, user: users[7])
-FactoryBot.create_list(:item, 2, user: users[8])
-FactoryBot.create_list(:item, 5, user: users[9])
+# FactoryBot.create_list(:item, 5, user: users[0])
+# FactoryBot.create_list(:item, 8, user: users[1])
+# FactoryBot.create_list(:item, 3, user: users[2])
+# FactoryBot.create_list(:item, 5, user: users[3])
+# FactoryBot.create_list(:item, 1, user: users[4])
+# FactoryBot.create_list(:item, 5, user: users[5])
+# FactoryBot.create_list(:item, 6, user: users[6])
+# FactoryBot.create_list(:item, 5, user: users[7])
+# FactoryBot.create_list(:item, 2, user: users[8])
+# FactoryBot.create_list(:item, 5, user: users[9])
 
-FactoryBot.create_list(:trip, 3)
+# FactoryBot.create_list(:trip, 3)
+User.create!{ first_name: "something", last_name:"this", email: "email@email.com"}
+User.create!{ first_name: "asda", last_name:"this", email: "cheese@email.com"}
+User.create!{ first_name: "monkey", last_name:"this", email: "foo@email.com"}
