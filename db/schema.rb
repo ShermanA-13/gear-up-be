4681a4b2e87c38da4dced1c8366586a21956ad3f
Susan Boyd <sueboyd922@gmail.com>
2022-06-04 17:35:20 -0400

    Feat: rake task created, climbing areas json added to db folder

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_171355) do
+ActiveRecord::Schema.define(version: 2022_06_04_204302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "areas", force: :cascade do |t|
+    t.string "name"
+    t.string "state"
+    t.string "url"
+    t.string "long"
+    t.string "lat"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
---
+++ spec/factories/areas.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :area do
+    name { "MyString" }
+    state { "MyString" }
+    url { "MyString" }
+    long { "MyString" }
+    lat { "MyString" }
+  end
+end
