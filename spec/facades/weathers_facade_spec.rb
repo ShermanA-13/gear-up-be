d6d61a0ee6e57c1d09a094ced7e99455fc7ad048
Phillip Kamps <phillip.kamps@gmail.com>
2022-06-06 14:57:05 -0600

    Misc bug fixes

--- spec/facades/weathers_facade_spec.rb
+++ spec/facades/weathers_facade_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe WeathersFacade do
- it ".get_data" do
-   weathers = WeathersFacade.get_weather(35,139)
+  it ".get_data" do
+    weathers = WeathersFacade.get_weather(35, 139)
 
-   expect(weathers).to be_a(Array)
-   weathers.each do |weather|
+    expect(weathers).to be_a(Array)
+    weathers.each do |weather|
       expect(weather).to be_a Weather
     end
   end
