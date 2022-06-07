c1cc682dfc9a8d9becd0bc868ac65f7ce4f9eea3
Susan Boyd <sueboyd922@gmail.com>
2022-06-07 14:45:38 -0400

    Feat: Add endpoint for area show

--- app/controllers/api/v1/areas_controller.rb
+++ app/controllers/api/v1/areas_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::AreasController < ApplicationController
   def find_all
     render json: AreaSerializer.new(Area.find_all_by_name(params[:name]))
   end
+
+  def show
+    area = Area.find(params[:id])
+    render json: AreaSerializer.new(area)
+  end
 end
