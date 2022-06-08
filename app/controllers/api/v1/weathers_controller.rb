04d819cbe27c35c3f393a2a8d12560780df7c9b2
Sherman Au <shermau13@gmail.com>
2022-06-08 11:23:04 -0600

    feat: add .set_area before_action

--- app/controllers/api/v1/weathers_controller.rb
+++ app/controllers/api/v1/weathers_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::WeathersController < ApplicationController
+  before_action :set_area, only: [:index]
+
 
   def index
-    weather = WeathersFacade.get_weather(location.lat, location.long)
-    render json: WeatherSerializer.new(weather)
+      weather = WeathersFacade.get_weather(location.lat, location.long)
+      render json: WeatherSerializer.new(weather)
   end
 
   private
