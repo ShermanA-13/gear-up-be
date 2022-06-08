4875a2d5557cad3c50eba233c904456066911a34
Susan Boyd <sueboyd922@gmail.com>
2022-06-08 10:54:13 -0400

    Refactor: set_trip before action in trip_users

--- app/controllers/api/v1/trip_users_controller.rb
+++ app/controllers/api/v1/trip_users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::TripUsersController < ApplicationController
+  before_action :set_trip, only: [:create, :update]
   def index
     users = User.users_on_trip(params[:trip_id])
     render json: UserSerializer.new(users)
@@ -6,19 +7,19 @@ class Api::V1::TripUsersController < ApplicationController
 
   def create
     params[:users].each do |user|
-      TripUser.create(trip_id: params[:trip_id], user_id: user, host: false)
+      TripUser.create(trip_id: @trip.id, user_id: user, host: false)
     end
     render status: :created
   end
 
   def update
-    trip = Trip.find(params[:trip_id])
+    # trip = Trip.find(params[:trip_id])
     params[:users].each do |user|
       if !TripUser.exists?(user_id: user)
-        TripUser.create(trip_id: trip.id, user_id: user, host: false)
+        TripUser.create(trip_id: @trip.id, user_id: user, host: false)
       end
     end
-    trip.users_to_remove(params[:users]).each {|user| user.destroy}
+    @trip.users_to_remove(params[:users]).each {|user| user.destroy}
     render status: 200
   end
 end
