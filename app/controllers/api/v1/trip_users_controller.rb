e2de963962a2fe2a6d450b9f1ceb549e4877c1fe
Sherman Au <shermau13@gmail.com>
2022-06-09 22:51:24 -0600

    refactor: trip_id

--- app/controllers/api/v1/trip_users_controller.rb
+++ app/controllers/api/v1/trip_users_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::TripUsersController < ApplicationController
 
   def update
     params[:users].each do |user|
-      if !TripUser.exists?(user_id: user)
+      if !TripUser.exists?(user_id: user, trip_id: @trip.id)
         TripUser.create(trip_id: @trip.id, user_id: user, host: false)
       end
     end
