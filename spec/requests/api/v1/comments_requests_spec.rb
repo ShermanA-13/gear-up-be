1e57a160605eff4aacac7b3478b49f5f43dc2262
Susan Boyd <sueboyd922@gmail.com>
2022-06-23 17:53:32 -0400

    Test: Test set up for Comments

---
+++ spec/requests/api/v1/comments_requests_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Comments API requests" do
+  let!(:area) {create(:area)}
+  let!(:trip) { create(:trip, area: area) }
+  let!(:user_list) { create_list(:user, 2) }
+
+  describe "Creating a comment" do
+    it 'can create a comment' do
+      comment_params = {
+        trip_id: trip.id,
+        user_id: user_list.first.id,
+        message: "This is gonna be sick!"
+
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/comments", headers: headers, params: JSON.generate(comment: comment_params)
+
+      expect(response).to be_successful
+    end
+  end
+end
